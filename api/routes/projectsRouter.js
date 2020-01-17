dd5ad24bd4dc5d6f818274049101679f7764b18a
JonathanChen <jonny29009@yahoo.com>
2020-01-17 09:42:33 -0800

    add endpoints for put, post, delete, finish all project endpoints

--- api/routes/projectsRouter.js
+++ api/routes/projectsRouter.js
@@ -27,15 +27,31 @@ router.get('/:id/actions', verifyProjectId, (req, res) => {
 })
 
 router.post('/', verifyNewProject, (req, res) => {
-
+    const newProject = req.body;
+    db.insert(newProject).then(addedProject => {
+        res.status(201).json(addedProject)
+    }).catch(() => {
+        res.status(500).json({ message: "error while creating new project" })
+    })
 })
 
 router.put('/:id', verifyProjectId, verifyProjectUpdate, (req, res) => {
-
+    const updatingProject = req.body;
+    const { id } = req.project;
+    db.update(id, updatingProject).then(updatedProject => {
+        res.status(201).json(updatedProject)
+    }).catch(() => {
+        res.status(500).json({ message: "error while updating project" })
+    })
 })
 
 router.delete('/:id', verifyProjectId, (req, res) => {
-
+    const { id } = req.project;
+    db.remove(id).then(() => {
+        res.status(201).json({ message: "project has been deleted" })
+    }).catch(() => {
+        res.status(500).json({ message: "error while deleting project" })
+    })
 })
 
 module.exports = router;
