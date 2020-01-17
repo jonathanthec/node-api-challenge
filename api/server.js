ab19e07708a2230bf1ee4615ba591d3b6a95a869
JonathanChen <jonny29009@yahoo.com>
2020-01-17 08:37:53 -0800

    setup server, env variable, and index file

--- api/server.js
+++ api/server.js
@@ -1,4 +1,17 @@
 const express = require('express');
-
-
 const server = express();
+
+const actionsRouter = require('./routes/actionsRouter');
+const projectsRouter = require('./routes/projectsRouter');
+const logger = require('./middleware/logger');
+
+server.use(express.json());
+server.use(logger);
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
+
+server.get('/', (req, res) => {
+    res.json({ message: "server is up and running" })
+})
+
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,5 @@
+const server = require('./api/server');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
+})
